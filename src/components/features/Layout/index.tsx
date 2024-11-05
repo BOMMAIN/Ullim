38466136200161355be27a0c989d232f15f9ecc2
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-11-05 21:09:58 +0900

    env: 초기 세팅

---
+++ src/components/features/Layout/index.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <Outlet />
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+export default Layout;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const ContentWrapper = styled.div`
+  width: 40%;
+  height: 100%;
+  background-color: #eee;
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RouterPath } from "./path";
-import Introduce from "../pages/Introduce";
-import MyPage from "../pages/MyPage";
-import CommunityPage from "../pages/CommunityPage";
-import PostPage from "../pages/PostPage";
-import PostWritePage from "../pages/PostWritePage";
+import Introduce from "@pages/Introduce";
+import MyPage from "@pages/MyPage";
+import CommunityPage from "@pages/CommunityPage";
+import PostPage from "@pages/PostPage";
+import PostWritePage from "@pages/PostWritePage";
+import Layout from "@components/features/Layout";
 
 const router = createBrowserRouter([
-  { path: RouterPath.root, element: <Introduce /> },
-  { path: RouterPath.myPage, element: <MyPage /> },
-  { path: RouterPath.communityPage, element: <CommunityPage /> },
-  { path: RouterPath.postPage, element: <PostPage /> },
-  { path: RouterPath.postWritePage, element: <PostWritePage /> },
+  {
+    element: <Layout />,
+    children: [
+      { path: RouterPath.root, element: <Introduce /> },
+      { path: RouterPath.myPage, element: <MyPage /> },
+      { path: RouterPath.communityPage, element: <CommunityPage /> },
+      { path: RouterPath.postPage, element: <PostPage /> },
+      { path: RouterPath.postWritePage, element: <PostWritePage /> },
+    ],
+  },
 ]);
 
 export const Routes = () => {
